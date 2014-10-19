afbab3d740d3792f9d65943669725480ce52b794
mjw <mjwatson01@googlemail.com>
2014-10-19 21:02:37 +0100

    Complete homework 8

--- homework_8.hs
+++ homework_8.hs
@@ -1,12 +1,46 @@
 module Party where
 
 import Employee
+import Data.Monoid
+import Data.Tree
+import Data.List
 
 glCons :: Employee -> GuestList -> GuestList
 glCons employee (GL es fun) = GL (employee : es) (fun + empFun employee)
 
+instance Monoid GuestList where
+  mempty = GL [] 0
+  mappend (GL es fun) (GL es' fun') = GL (es ++ es') (fun + fun')
+
 moreFun :: GuestList -> GuestList -> GuestList
 moreFun a@(GL _ a') b@(GL _ b')
-  | a' < b' = b
-  | otherwise    = a
+  | a' < b'   = b
+  | otherwise = a
+
+treeFold :: (a -> [b] -> b) -> Tree a -> b
+treeFold f (Node a bs) = f a (map (treeFold f) bs)
+
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel boss gls = (bestWithBoss, bestWithoutBoss)
+  where bestWithBoss    = glCons boss $ mconcat $ map snd gls
+        bestWithoutBoss = mconcat $ map (uncurry moreFun) gls
+
+maxFun :: Tree Employee -> GuestList
+maxFun = (uncurry moreFun) . (treeFold nextLevel)
+
+processData :: String -> String
+processData contents = output
+  where employees        = read contents
+        guesslist        = maxFun employees
+        (GL invited fun) = guesslist
+        totalline        = "Total fun: " ++ (show fun)
+        invitedlist      = sort $ map empName invited
+        output           = unlines $ totalline : invitedlist
+
+
+processFile :: String -> IO ()
+processFile path = readFile path >>= (putStr . processData)
+
+main = processFile "company.txt"
+
 
